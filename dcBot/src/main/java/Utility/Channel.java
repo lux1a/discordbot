0b2cb869ea03a69b73864ebd5b8afc94f1e5d887
Lux <71046508+lux1a@users.noreply.github.com>
2020-12-08 21:59:12 +0100

    Update Channel.java

--- dcBot/src/main/java/Utility/Channel.java
+++ dcBot/src/main/java/Utility/Channel.java
@@ -26,19 +26,4 @@ public class Channel extends ListenerAdapter {
 
         }
     }
-
-    private MessageEmbed createEmbed(MessageReceivedEvent e) {
-        EmbedBuilder embedBuilder = new EmbedBuilder();
-        Object[] userArray = e.getMessage().getMentionedUsers().toArray();
-        User firstMention = (User) userArray[0];
-        firstMention.getName();
-        embedBuilder.setTitle(firstMention.getName().replace(firstMention.getName().charAt(0), firstMention.getName().toUpperCase().charAt(0))+ ":");
-        embedBuilder.setThumbnail(firstMention.getAvatarUrl());
-        embedBuilder.setDescription("**Account created: ** " + "`" + firstMention.getTimeCreated().getDayOfMonth() + "." + firstMention.getTimeCreated().getMonthValue() + "." + firstMention.getTimeCreated().getYear() + "`" + "\n"
-                + "**ID:** " + "`" + firstMention.getId() + "`" + "\n" +"**Discriminator: ** " + "`" + firstMention.getDiscriminator() + "`" + "\n" +
-                "**Bot?: ** " + "`" + firstMention.isBot() + "`");
-        embedBuilder.setColor(Color.decode("#db273a"));
-        embedBuilder.setFooter("Query by: " + e.getAuthor().getName().replace(e.getAuthor().getName().charAt(0), e.getAuthor().getName().toUpperCase().charAt(0)), e.getAuthor().getAvatarUrl());
-        return embedBuilder.build();
-    }
 }
