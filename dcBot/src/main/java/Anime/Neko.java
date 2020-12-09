40ebba0c21edf3be42cfeb42ad11bc727d761601
Daniel N <danienik@student.ethz.ch>
2020-12-09 01:42:01 +0100

    Trying to fix _neko

--- dcBot/src/main/java/Anime/Neko.java
+++ dcBot/src/main/java/Anime/Neko.java
@@ -3,7 +3,10 @@ package Anime;
 import Main.Main;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.json.JSONObject;
+
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Arrays;
 
 public class Neko extends Json {
@@ -14,17 +17,28 @@ public class Neko extends Json {
     public void onMessageReceived(MessageReceivedEvent e) {
         if (e.getAuthor().isBot()) {
         } else if (e.getMessage().getContentRaw().equals(Main.prefix + "neko")) {
-            nekoText(e);
+            try {
+                nekoText(e);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         } else if (Arrays.asList(wrongSpelling).contains(Main.prefix + e.getMessage().getContentRaw())) {
             e.getChannel().sendMessage("Did you mean **Neko**?").queue();
-            nekoText(e);
+            try {
+                nekoText(e);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
-    private void nekoText(MessageReceivedEvent e) {
+    private void nekoText(MessageReceivedEvent e) throws IOException {
+        URL url = new URL("https://neko-love.xyz/api/v1/neko");
         JSONObject json = null;
+        URLConnection conn = url.openConnection();
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
         try {
-            json = readJsonFromUrl("https://neko-love.xyz/api/v1/neko");
+            json = readJsonFromUrl(url.toString());
         } catch (IOException ex) {
             ex.printStackTrace();
         }
--- dcBot/src/main/java/Utility/Channel.java
+++ dcBot/src/main/java/Utility/Channel.java
@@ -1,44 +1,17 @@
 package Utility;
 
 import Main.Main;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.awt.*;
-
 public class Channel extends ListenerAdapter {
-
 
     public void onMessageReceived(MessageReceivedEvent e) {
         MessageChannel channel = e.getChannel();
         if (e.getAuthor().isBot()) {
         } else if (e.getMessage().getContentRaw().equals(Main.prefix + "channel")) {
             e.getChannel().sendMessage("Current Channel: " + "`" + channel + "`").queue();
-        } else if (e.getMessage().getContentRaw().equals(Main.prefix + "user")) {
-            e.getChannel().sendMessage("Please mention a user.").queue();
-        } else if (e.getMessage().getContentRaw().startsWith(Main.prefix + "user") && e.getMessage().getContentRaw().contains("@")) {
-            e.getChannel().sendMessage(createEmbed(e)).queue();
-
         }
-    }
-
-    private MessageEmbed createEmbed(MessageReceivedEvent e) {
-        EmbedBuilder embedBuilder = new EmbedBuilder();
-        Object[] userArray = e.getMessage().getMentionedUsers().toArray();
-        User firstMention = (User) userArray[0];
-        firstMention.getName();
-        embedBuilder.setTitle(firstMention.getName().replace(firstMention.getName().charAt(0), firstMention.getName().toUpperCase().charAt(0))+ ":");
-        embedBuilder.setThumbnail(firstMention.getAvatarUrl());
-        embedBuilder.setDescription("**Account created: ** " + "`" + firstMention.getTimeCreated().getDayOfMonth() + "." + firstMention.getTimeCreated().getMonthValue() + "." + firstMention.getTimeCreated().getYear() + "`" + "\n"
-                + "**ID:** " + "`" + firstMention.getId() + "`" + "\n" +"**Discriminator: ** " + "`" + firstMention.getDiscriminator() + "`" + "\n" +
-                "**Bot?: ** " + "`" + firstMention.isBot() + "`");
-        embedBuilder.setColor(Color.decode("#db273a"));
-        embedBuilder.setFooter("Query by: " + e.getAuthor().getName().replace(e.getAuthor().getName().charAt(0), e.getAuthor().getName().toUpperCase().charAt(0)), e.getAuthor().getAvatarUrl());
-        return embedBuilder.build();
     }
 }
