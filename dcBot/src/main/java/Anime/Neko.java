8fb83f918a43fc81ea7fdc677557860b9be61a7e
Daniel N <danienik@student.ethz.ch>
2020-12-09 01:55:17 +0100

    Replacing JSON Library

--- dcBot/src/main/java/Anime/Neko.java
+++ dcBot/src/main/java/Anime/Neko.java
@@ -1,10 +1,14 @@
 package Anime;
 
 import Main.Main;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.json.JSONObject;
-
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Arrays;
@@ -33,16 +37,28 @@ public class Neko extends Json {
     }
 
     private void nekoText(MessageReceivedEvent e) throws IOException {
-        URL url = new URL("https://neko-love.xyz/api/v1/neko");
-        JSONObject json = null;
-        URLConnection conn = url.openConnection();
-        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+       /* JSONObject json = null;
         try {
-            json = readJsonFromUrl(url.toString());
+            json = readJsonFromUrl("https://neko-love.xyz/api/v1/neko");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
         assert json != null;
-        e.getChannel().sendMessage(json.get("url").toString()).queue();
+        e.getChannel().sendMessage(json.get("url").toString()).queue();*/
+
+        String sURL = "https://neko-love.xyz/api/v1/neko";
+        URL url = new URL(sURL);
+        URLConnection request = url.openConnection();
+        request.setRequestProperty("User-Agent", "Mozilla/5.0");
+        request.connect();
+
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject rootobj = root.getAsJsonObject();
+        String nekoUrl = rootobj.get("url").getAsString();
+        e.getChannel().sendMessage(nekoUrl).queue();
+
     }
+
+
 }
