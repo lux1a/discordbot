a7680e800db083af0c754c074e16df6c616ca5b3
Daniel N <danienik@student.ethz.ch>
2020-12-10 18:43:18 +0100

    fix

--- dcBot/src/main/java/Anime/Neko.java
+++ dcBot/src/main/java/Anime/Neko.java
@@ -4,7 +4,6 @@ import Main.Main;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
