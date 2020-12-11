17e0c2add00b2e86d62061b1a8ae442f6dcccdd8
Daniel N <danienik@student.ethz.ch>
2020-12-11 09:06:42 +0100

    Update _help

--- dcBot/src/main/java/Essentials/Help.java
+++ dcBot/src/main/java/Essentials/Help.java
@@ -26,8 +26,8 @@ public class Help extends ListenerAdapter {
     private MessageEmbed createEmbed() {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setTitle("Available Commands:");
-        embedBuilder.setDescription("" + "```ARM\n" + "_neko: Catgirl Image (Currently not working) \n" + "```" + "```ARM\n" + "_waifu: Waifu Image (Currently not working)\n" + "```" + "```ARM\n" +
-         "_kitsune: Foxgirl Image (Currently not working)\n" + "```" + "```ARM\n" + "_info: Shows info\n" + "```"  + "```ARM\n" + "_ping: pong!\n" + "```" + "```ARM\n" + "_vanish: Message vanishes after one second\n" +
+        embedBuilder.setDescription("" + "```ARM\n" + "_neko: Catgirl Image\n" + "```" + "```ARM\n" + "_waifu: Waifu Image\n" + "```" + "```ARM\n" +
+         "_kitsune: Foxgirl Image\n" + "```" + "```ARM\n" + "_info: Shows info\n" + "```"  + "```ARM\n" + "_ping: pong!\n" + "```" + "```ARM\n" + "_vanish: Message vanishes after one second\n" +
           "```" + "```ARM\n" + "_bee: Bee Movie Script (works once every 50min)\n" + "```" + "```ARM\n" + "_help: this message\n" + "```" + "```ARM\n" + "_kurumi: Kurumi Picture\n" + "```" + "```ARM\n" + "_kurumigif: Kurumi Gif)\n" + "```"
         + "```ARM\n" + "_crypto: Price Data for any cryptocurrency\n" + "```" + "```ARM\n" + "_ascii: _ascii help for more info\n" + "```" + "```ARM\n" + "_user: Mentioned Users Info\n" + "```");
         embedBuilder.setAuthor("Luxia's Bot", "https://bit.ly/3fKaDDv", "https://cdn.discordapp.com/avatars/684629625313558557/1705ee73c703b8b3343a29acd24973b3.png");
